1391233db66685e187668ec33c2ce581069232f7
Tao Wen <taowen@chengfayun.com>
2021-03-11 21:52:00 +0800

    add Future

---
+++ src/should.ts
@@ -0,0 +1,14 @@
+import { Impl, newTrace, Scene } from "@stableinf/io";
+
+export function should(behavior: string, func: (scene: Scene) => void) {
+    return async function(this: any) {
+        const scene = new Scene(newTrace('test'), {
+            database: new Impl.InMemDatabase(),
+            serviceProtocol: new Impl.HttpRpcClient(),
+        });
+        scene.onAtomChanged = (atom) => {
+            atom.onAtomChanged(scene.span);
+        }
+        return scene.execute(this, func);
+    };
+}
